f6fb82c84495a3945b24976fedc12ff001f4aecb
whizzbbig <54703305+whizzbbig@users.noreply.github.com>
2021-10-26 10:06:46 +0530

    Integrating Prismic with your project

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
+/* eslint-disable no-unused-vars */
+require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = 8004;
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+  });
+};
+
+// Link Resolver
+const HandleLinkResolver = (doc) => {
+  // Define the url depending on the document type
+  //   if (doc.type === 'page') {
+  //     return '/page/' + doc.uid;
+  //   } else if (doc.type === 'blog_post') {
+  //     return '/blog/' + doc.uid;
+  //   }
+
+  // Default to homepage
+  return '/';
+};
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: HandleLinkResolver,
+  };
+
+  res.locals.PrismicDOM = PrismicDOM;
+
+  next();
+});
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -12,16 +50,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.render('pages/about')
-})
+  initApi(req).then((api) => {
+    api
+      .query(Prismic.Predicates.any('document.type', ['meta', 'about']))
+      .then((response) => {
+        const { results } = response;
+        const [about, meta] = results;
+        console.log(about, meta);
+        res.render('pages/about', {
+          meta,
+          about,
+        });
+      });
+  });
+});
 
-app.get('/detail/:', (req, res) => {
-    res.render('pages/detail')
-})
+app.get('/detail/:uid', (req, res) => {
+  res.render('pages/detail');
+});
 
-app.get('/collections', (req, res) => {
-    res.render('pages/collections')
-})
+app.get('/collection', (req, res) => {
+  res.render('pages/collection');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
